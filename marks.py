0019892c4cbd2e16aa1f8f8e963baaa3029a13ba
Dana <danatroshina@mail.ru>
2023-03-21 18:25:30 +0300

    Кнопка  дз и оценок

--- marks.py
+++ marks.py
@@ -17,8 +17,10 @@ def auth(login, par, pr='https://edu.tatar.ru/user/diary/week'):
         'Cache-Control': 'no-cache',
         'Upgrade-Insecure-Requests': '1',
         'Origin': 'https://edu.tatar.ru',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
+                      '537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/'
+                  'webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Sec-GPC': '1',
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-Mode': 'navigate',
@@ -29,19 +31,19 @@ def auth(login, par, pr='https://edu.tatar.ru/user/diary/week'):
     }
     data = {
         'main_login2': f'{login}',
-        'main_password2': f'{par}',}
+        'main_password2': f'{par}', }
     response = session.post('https://edu.tatar.ru/logon', headers=headers, data=data, allow_redirects=True)
     pr_res = session.get(pr, headers=headers)
     return pr_res
 
 
-def marks(log, par, ur='https://edu.tatar.ru/user/diary/week'):
+def marks(log, par, ur=url_active, obj=None):
     result = []
     pr_res = auth(log, par, ur)
     soup = BeautifulSoup(pr_res.text, "html.parser")
-    marks = soup.find_all(class_='tt-mark')
+    all_marks = soup.find_all(class_='tt-mark')
     mark = []
-    for data in marks:
+    for data in all_marks:
         if data.find('div') is not None:
             mark.append(data.text)
 
@@ -51,11 +53,11 @@ def marks(log, par, ur='https://edu.tatar.ru/user/diary/week'):
         if data.find('div') is not None:
             subject.append(data.text)
 
-    #hometasks = soup.find_all(class_='tt-task')
-    #hometask = []
-    #for data in hometasks:
-    #    if data.find('div') is not None:
-    #        hometask.append(data.text)
+    all_hometasks = soup.find_all(class_='tt-task')
+    hometask = []
+    for data in all_hometasks:
+        if data.find('div') is not None:
+            hometask.append(data.text)
 
     dates = []
     day_numb = 1
@@ -66,7 +68,7 @@ def marks(log, par, ur='https://edu.tatar.ru/user/diary/week'):
             dates.append(w.text)
 
     d = soup.find_all(class_="tt-days-mo")
-    if d != []:
+    if d is not None:
         week = 1
     else:
         week = 2
@@ -86,12 +88,12 @@ def marks(log, par, ur='https://edu.tatar.ru/user/diary/week'):
                 day_numb = 1
 
         if subject[i].strip() != '':
-            result.append(str(less) + ')' + subject[i].strip() + ': ')
-            result.append(mark[i].strip() if mark[i] != '\n\n' else '')
+            result.append(str(less) + ')' + subject[i].strip())
+            if obj == 'mark':
+                result.append(': ' + mark[i].strip() if mark[i] != '\n\n' else '')
+            elif obj == 'task':
+                result.append(': ' + hometask[i].strip())
             result.append('\n')
-            #result.append("Д/З: " + hometask[i].strip())
-            #result.append("Оценка: " + mark[i].strip() if mark[i] != '\n\n' else '-')
-            result.append('')
         less += 1
     return ''.join(result)
 
@@ -101,10 +103,12 @@ def next_w(log, par):
     pr_res = auth(log, par, url_active)
     soup = BeautifulSoup(pr_res.text, "html.parser")
     next_week = soup.find_all(class_="g-button-blue")
-    new_url = str(next_week[1]['href'])
-    url_active = new_url
-    print(url_active)
-    return marks(log, par, url_active)
+    try:
+        new_url = str(next_week[1]['href'])
+        url_active = new_url
+        return marks(log, par, url_active)
+    except:
+        return 'Еще недоступно'
 
 
 def last_w(log, par):
@@ -114,5 +118,4 @@ def last_w(log, par):
     next_week = soup.find_all(class_="g-button-blue")
     new_url = str(next_week[0]['href'])
     url_active = new_url
-    print(url_active)
     return marks(log, par, url_active)
--- server.py
+++ server.py
@@ -13,13 +13,14 @@ TOKEN = "5379058684:AAFD4kCxjJDJrhVuqAdbYbceNlGD3nasObw"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
 inline_kb_full = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton('Мои оценки', callback_data='btn1'))\
     .add(InlineKeyboardButton('Расписание звонков', callback_data='btn2'))\
     .add(InlineKeyboardButton('Расписание уроков', callback_data='btn3'))
 
 inline_kb_nl = InlineKeyboardMarkup(row_width=2).add(InlineKeyboardButton('Следущая неделя', callback_data='next'))\
     .add(InlineKeyboardButton('Предыдущая неделя', callback_data='last'))\
+    .add(InlineKeyboardButton('Показать оценки', callback_data='btn4'))\
+    .add(InlineKeyboardButton('Показать домашнее задание', callback_data='btn5'))\
     .add(InlineKeyboardButton('Назад', callback_data='n'))
 
 
@@ -33,40 +34,23 @@ async def send_welcome(message: types.Message):
 async def process_callback_button1(callback_query: types.CallbackQuery):
     if login == '':
         await bot.answer_callback_query(callback_query.id)
-        await bot.send_message(callback_query.from_user.id, 'Введите логин в формате\n/login *ваш логин*')
+        await bot.send_message(callback_query.from_user.id, 'Введите логин в формате\n/log *ваш логин*')
     else:
-        marks.marks(login, password)
+        await bot.answer_callback_query(callback_query.id)
+        await bot.send_message(callback_query.from_user.id, marks.marks(login, password), reply_markup=inline_kb_nl)
 
 
-@dp.message_handler(commands=['login'])
+@dp.message_handler(commands=['log'])
 async def cmd_name(message: types.Message, command: {}):
     global login
     if command.args:
         login = command.args
-        await message.answer("Введите пароль в формате\n/password *ваш пароль*")
+        await message.answer("Введите пароль в формате\n/pas *ваш пароль*")
     else:
-        await message.answer("Пожалуйста, укажи свой логин после команды /passsword!")
+        await message.answer("Пожалуйста, укажи свой логин после команды /log!")
 
 
-@dp.callback_query_handler(lambda c: c.data == 'last')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, marks.last_w(login, password), reply_markup=inline_kb_nl)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'next')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, marks.next_w(login, password), reply_markup=inline_kb_nl)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'n')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, "Что тебе нужно?", reply_markup=inline_kb_full)
-
-
-@dp.message_handler(commands=['password'])
+@dp.message_handler(commands=['pas'])
 async def cmd_name(message: types.Message, command: {}):
     global password
     if command.args:
@@ -76,7 +60,41 @@ async def cmd_name(message: types.Message, command: {}):
         except:
             await message.answer("Пожалуйста, укажи верные данные для edu.tatr\nПопробуй снова")
     else:
-        await message.answer("Пожалуйста, укажи свой логин после команды /login!")
+        await message.answer("Пожалуйста, укажи свой пароль после команды /pas!")
+
+
+@dp.callback_query_handler(lambda c: c.data == 'last')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, marks.last_w(login, password),
+                           reply_markup=inline_kb_nl)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'next')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, marks.next_w(login, password),
+                           reply_markup=inline_kb_nl)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'btn4')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, marks.marks(login, password, obj='mark'),
+                           reply_markup=inline_kb_nl)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'btn5')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, marks.marks(login, password, obj='task'),
+                           reply_markup=inline_kb_nl)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'n')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, "Что тебе нужно?", reply_markup=inline_kb_full)
 
 
 @dp.callback_query_handler(lambda c: c.data == 'btn2')
